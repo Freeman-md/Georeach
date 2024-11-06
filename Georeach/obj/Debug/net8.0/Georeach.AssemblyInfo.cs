bdd2bc353d4b904eeabcbbca579256fa85e2a6ce
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2024-11-06 15:18:13 +0000

    add web.config to blazor project

--- Georeach/obj/Debug/net8.0/Georeach.AssemblyInfo.cs
+++ Georeach/obj/Debug/net8.0/Georeach.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Georeach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+487b3471744ea847129cb83662f563dce023e40e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44190fa9be70fbfb77d60d108174c475972d55a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Georeach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Georeach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
